7f9f3538fb9c22a2bc4e17729a0ce196ae4ebb15
CimimUxMaio <cimimuxmaio@gmail.com>
2021-12-07 00:48:01 -0300

    Better queue card

--- src/components/QueueCard.jsx
+++ src/components/QueueCard.jsx
@@ -7,7 +7,7 @@ const QueueCard = ({name}) => {
       <Card className="queue-card" border="primary">
         <Card.Body>
           <Card.Title>{name}</Card.Title>
-          <Card.Text>{`Una cola llamada ${name}`}</Card.Text>
+          <Card.Text>{`Una cola llamada "${name}"`}</Card.Text>
         </Card.Body>
       </Card>
     </Link>
